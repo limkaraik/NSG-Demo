6b8ee2ea5799e26521e99dbbae91dde2666971b6
Kar Aik Lim <limkaraik@hotmail.com>
2021-10-07 13:51:56 +0800

    edit image url

--- src/common/common.ts
+++ src/common/common.ts
@@ -1,22 +1,22 @@
 import {Icon} from "leaflet";
 
 export const Robber = new Icon({
-    iconUrl: "/robber.png",
+    iconUrl: "/NSG-Demo/robber.png",
     iconSize: [25, 25]
   });
 
 export const Jail = new Icon({
-iconUrl: "/jail.png",
+iconUrl: "/NSG-Demo/jail.png",
 iconSize: [25, 25]
 });
 
 export const Police = new Icon({
-    iconUrl: "/police.png",
+    iconUrl: "/NSG-Demo/police.png",
     iconSize: [25, 25]
     });
 
 export const Exit = new Icon({
-  iconUrl: "/exit.png",
+  iconUrl: "/NSG-Demo/exit.png",
   iconSize: [25, 25]
   });
 
